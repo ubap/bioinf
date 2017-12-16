bde044f784b36cece24f4e780f6d2092bb7d365d
Jakub Trzebiatowski <jtrzebiatowski@soldevelo.com>
2017-12-16 11:18:37 +0100

    Add biojava-core dependency

--- src/main/java/pl/edu/pg/eti/bioinf/Starter.java
+++ src/main/java/pl/edu/pg/eti/bioinf/Starter.java
@@ -1,8 +1,15 @@
 package pl.edu.pg.eti.bioinf;
 
+import org.biojava.nbio.core.exceptions.CompoundNotFoundException;
+import org.biojava.nbio.core.sequence.DNASequence;
+import org.biojava.nbio.core.sequence.ProteinSequence;
+
 public class Starter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CompoundNotFoundException {
+        ProteinSequence proteinSequence = new ProteinSequence("ARNDCEQGHILKMFPSTWYVBZJX");
+        DNASequence dnaSequence = new DNASequence("ATCG");
+
         System.out.println("hello worlds");
     }
 }
