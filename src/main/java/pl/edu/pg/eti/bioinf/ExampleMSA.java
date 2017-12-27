cdca3e10a57c5191c001a32b42c17cf7c2732f84
marta15 <mazur.marta1@gmail.com>
2017-12-27 17:14:00 +0100

    Modifying aligned sequence from the generated profile

--- src/main/java/pl/edu/pg/eti/bioinf/ExampleMSA.java
+++ src/main/java/pl/edu/pg/eti/bioinf/ExampleMSA.java
@@ -3,14 +3,20 @@ package pl.edu.pg.eti.bioinf;
 import org.biojava.nbio.alignment.Alignments;
 import org.biojava.nbio.alignment.SimpleGapPenalty;
 import org.biojava.nbio.alignment.template.GapPenalty;
+import org.biojava.nbio.core.alignment.SimpleAlignedSequence;
+import org.biojava.nbio.core.alignment.template.AlignedSequence;
+import org.biojava.nbio.core.alignment.template.MutableProfile;
+import org.biojava.nbio.core.alignment.template.MutableProfilePair;
 import org.biojava.nbio.core.alignment.template.Profile;
 import org.biojava.nbio.core.sequence.ProteinSequence;
 import org.biojava.nbio.core.sequence.compound.AminoAcidCompound;
 import org.biojava.nbio.core.sequence.io.FastaReaderHelper;
+import org.biojava.nbio.core.sequence.template.Sequence;
 import org.biojava.nbio.core.util.ConcurrencyTools;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ExampleMSA {
@@ -35,7 +41,7 @@ public class ExampleMSA {
         Alignments.PairwiseSequenceScorerType psst = Alignments.PairwiseSequenceScorerType.GLOBAL_IDENTITIES;
 
         GapPenalty gapPenalty = new SimpleGapPenalty(1 , 3);
-        System.out.println(gapPenalty.getType());
+        // System.out.println(gapPenalty.getType());
 
         // also GLOBAL_CONSENSUS, GLOBAL_LINEAR_SPACE, LOCAL, LOCAL_CONSENSUS, LOCAL_LINEAR_SPACE
         Alignments.ProfileProfileAlignerType ppat = Alignments.ProfileProfileAlignerType.GLOBAL;
@@ -43,14 +49,31 @@ public class ExampleMSA {
         // ----------------->
 
         Profile<ProteinSequence, AminoAcidCompound> profile = Alignments.getMultipleSequenceAlignment(lst, psst, gapPenalty, ppat);
-        System.out.printf("Clustalw:%n%s%n", profile);
+       // MutableProfile<ProteinSequence, AminoAcidCompound> mProfile = (MutableProfilePair<ProteinSequence, AminoAcidCompound>)Alignments.getMultipleSequenceAlignment(lst, psst, gapPenalty, ppat);
+        int idx = 2;
+        List<AlignedSequence<ProteinSequence, AminoAcidCompound>> sequences = profile.getAlignedSequences();
+        System.out.println(sequences.get(idx)+" length: "+sequences.size());
+        List<AlignedSequence<ProteinSequence, AminoAcidCompound>> sequencesCp = new ArrayList<AlignedSequence<ProteinSequence, AminoAcidCompound>>();
+        for (AlignedSequence<ProteinSequence, AminoAcidCompound> seq : sequences) {
+            sequencesCp.add(seq);
+        }
+        AlignedSequence<ProteinSequence, AminoAcidCompound> newSeq, prev;
+        List<AlignedSequence.Step> steps;
+        prev = sequencesCp.get(idx);
+        steps = new ArrayList<AlignedSequence.Step>(Collections.nCopies(prev.getLength(), AlignedSequence.Step.COMPOUND));
+        steps.add(5, AlignedSequence.Step.GAP);
+        newSeq = new SimpleAlignedSequence<ProteinSequence, AminoAcidCompound>(prev, steps);
+        sequencesCp.set(idx, newSeq);
+        System.out.println(sequencesCp.get(idx)+" length: "+sequencesCp.size());
+
+        // System.out.printf("Clustalw:%n%s%n", profile);
         ConcurrencyTools.shutdown();
     }
 
     private static ProteinSequence getSequenceForId(String uniProtId) throws Exception {
         URL uniprotFasta = new URL(String.format("http://www.uniprot.org/uniprot/%s.fasta", uniProtId));
         ProteinSequence seq = FastaReaderHelper.readFastaProteinSequence(uniprotFasta.openStream()).get(uniProtId);
-        System.out.printf("id : %s %s%n%s%n", uniProtId, seq, seq.getOriginalHeader());
+        // System.out.printf("id : %s %s%n%s%n", uniProtId, seq, seq.getOriginalHeader());
         return seq;
     }
 }
